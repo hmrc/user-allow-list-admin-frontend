c19a563a939e7955a1a12c95dfb897875718ffa5
Mike Burns <13621631+burns238@users.noreply.github.com>
2026-03-12 09:52:24 +0000

    Update bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"   % "12.31.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"   % "12.32.0",
     "uk.gov.hmrc"             %% "internal-auth-client-play-30" % "4.3.0"
   )
 
